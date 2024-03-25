ddd81c378d239ff53d2a937e98458627f676a21a
nietzid <arfiansyah@upi.edu>
2024-03-25 13:17:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+// import Button from "remoteApp/Button";
+// import App from 
+const {App} = await import("ideaApp/App");
+// import useStore from "remoteApp/store";
+
+function Apps() {
+  // const [count, setCount] = useStore();
+
+  return (
+    <>
+      {/* <div className="App">
+        <h1>Host Application</h1>
+        <Button />
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </div> */}
+      <App />
+    </>
+  );
+}
+
+export default App;
